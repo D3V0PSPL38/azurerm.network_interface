8e434ce4f78442d630246509872f00b835f58428
RalphBrynard <ralph@thebrynards.com>
2023-06-20 19:38:14 -0500

    Title: main.tf
    Body: Basic structure for main.tf
    
    Co-authored-by: Ralph Brynard <ralph@thebrynards.com>

--- main.tf
+++ main.tf
@@ -1,2 +1,35 @@
 ### [BEGIN] main.tf ###
+resource "time_static" "deploy_date" {}
+
+module "label" {
+  source          = "github.com/D3V0PSPL38/terraform-context-label.git"
+  enabled         = module.this.enabled
+  name            = var.name
+  namespace       = var.namespace
+  environment     = var.environment
+  stage           = var.stage
+  location        = var.location
+  label_order     = var.label_order
+  id_length_limit = var.id_length_limit
+  tags            = var.tags
+  context         = module.this.context
+}
+
+resource "azurerm_network_interface" "this" {
+  count = local.e ? 1 : 0
+  name = format("%s-%02d", module.label.id, count.index + 1)
+  resource_group_name = var.resource_group_name
+  dynamic "ip_configuration" {
+    for_each = var.ip_configuration
+    iterator = ip
+    content {
+      name = ip.value.name
+      subnet_id = ip.value.subnet_id
+      private_ip_address_allocation = ip.value.private_ip_address_allocation
+      private_ip_address_version = ip.value.private_ip_address_version
+      private_ip_address = ip.value.private_ip_address
+    }
+  }
+  tags = module.label.tags
+}
 ### [END] main.tf ###
--- variables.tf
+++ variables.tf
@@ -1,2 +1,29 @@
 ### [BEGIN] variables.tf ###
+variable "resource_group_name" {
+  description = <<EOD
+  [Optional] The name of an existing resource group in which to provision the virtual network. If ommitted, a new resource group will be created.
+  EOD
+  type        = string
+  default     = null
+}
+
+variable "ip_configuration" {
+  description = <<EOD
+  [Optional] Network interface IP configuration. Changing this forces a new resource to be created.
+  EOD
+  type = object({
+    name                          = optional(string)
+    subnet_id                     = optional(string)
+    private_ip_address_allocation = optional(string)
+    private_ip_address_version    = optional(string)
+    private_ip_address            = optional(string)
+  })
+  default = {
+    name                          = null
+    subnet_id                     = null
+    private_ip_address_allocation = null
+    private_ip_address_version    = null
+    private_ip_address            = null
+  }
+}
 ### [END] variables.tf ###
