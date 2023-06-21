3025992518e87ee6f6fe6431d79889e7e153ba66
RalphBrynard <ralph@thebrynards.com>
2023-06-20 19:40:08 -0500

    Title: main.tf
    Body: Basic structure for main.tf
    
    Co-authored-by: Ralph Brynard <ralph@thebrynards.com>

--- main.tf
+++ main.tf
@@ -16,18 +16,19 @@ module "label" {
 }
 
 resource "azurerm_network_interface" "this" {
-  count = local.e ? 1 : 0
-  name = format("%s-%02d", module.label.id, count.index + 1)
+  count               = local.e ? 1 : 0
+  name                = format("%s-%02d", module.label.id, count.index + 1)
   resource_group_name = var.resource_group_name
+  location            = var.location
   dynamic "ip_configuration" {
     for_each = var.ip_configuration
     iterator = ip
     content {
-      name = ip.value.name
-      subnet_id = ip.value.subnet_id
+      name                          = ip.value.name
+      subnet_id                     = ip.value.subnet_id
       private_ip_address_allocation = ip.value.private_ip_address_allocation
-      private_ip_address_version = ip.value.private_ip_address_version
-      private_ip_address = ip.value.private_ip_address
+      private_ip_address_version    = ip.value.private_ip_address_version
+      private_ip_address            = ip.value.private_ip_address
     }
   }
   tags = module.label.tags
